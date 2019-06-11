ad534193ad7caedb07396968ae7bc90d2e9b6ed7
syh80881 <syh8088@gmail.com>
2019-06-11 23:15:43 +0900

    kiwi blog start!!
    kiwi blog start!!

---
+++ src/main/java/kiwi/blog/KiwiBlogApplication.java
@@ -0,0 +1,13 @@
+package kiwi.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KiwiBlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KiwiBlogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kiwi/blog/KiwiBlogApplicationTests.java
@@ -0,0 +1,16 @@
+package kiwi.blog;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class KiwiBlogApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
