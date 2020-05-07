99b96a8896e179371282f6a7af6ad16a6dd50f1b
syh80881 <syh8088@gmail.com>
2020-05-07 13:39:12 +0900

    deploy
    deploy

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,30 +1,33 @@
 #!/bin/bash
-BUILD_PATH=$(ls /home/ubuntu/app/kiwi/step2/zip/*.jar)
+
+REPOSITORY=/home/ubuntu/app/kiwi/step2
+PROJECT_NAME=kiwi
+
 JAR_NAME=$(basename $BUILD_PATH)
 echo "> build 파일명: $JAR_NAME"
 
-echo "> build 파일 복사"
-DEPLOY_PATH=/home/ubuntu/app/kiwi/
+echo "> Build 파일 복사"
 
-cp $BUILD_PATH $DEPLOY_PATH
+cp $REPOSITORY/zip/*.jar $REPOSITORY/
 
-echo "> 현재 실행중인 애플리케이션 pid 확인"
+echo "> 현재 구동중인 애플리케이션 pid 확인"
 
 CURRENT_PID=$(pgrep -fl $JAR_NAME | grep jar | awk '{print $1}')
-DEPLOY_JAR=$DEPLOY_PATH$JAR_NAME
 
-if [ -z $CURRENT_PID ]
-then
-  echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다."
+echo "현재 구동중인 어플리케이션 pid: $CURRENT_PID"
+
+if [ -z "$CURRENT_PID" ]; then
+    echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다."
 else
-  echo "> kill -15 $CURRENT_PID"
-  kill -15 $CURRENT_PID
-  sleep 5
+    echo "> kill -15 $CURRENT_PID"
+    kill -15 $CURRENT_PID
+    sleep 5
 fi
 
-echo "> $DEPLOY_JAR 배포"
+echo "> 새 어플리케이션 배포"
 
-JAR_NAME=$(ls -tr $DEPLOY_PATH*.jar | tail -n 1)
+JAR_NAME=$(ls -tr $REPOSITORY/*.jar | tail -n 1)
+
 echo "> JAR Name: $JAR_NAME"
 
 echo "> $JAR_NAME 에 실행권한 추가"
@@ -33,4 +36,6 @@ chmod +x $JAR_NAME
 
 echo "> $JAR_NAME 실행"
 
-nohup java -jar $DEPLOY_JAR , /home/ubuntu/app/kiwi/application-real-db.properties &
+nohup java -jar \
+    -Dspring.config.location=classpath:/application-real.yml,classpath:/home/ubuntu/app/kiwi/application-real1-db.properties,/home/ubuntu/app/kiwi/application-real-db.properties \
+    $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
